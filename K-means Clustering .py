d5242ca1eb0674ad3533e11cc2db4cf84f0ffa15
MachineLearningLibrary <69740045+MachineLearningLibrary@users.noreply.github.com>
2020-08-27 16:32:42 +1000

    Add files via upload

--- K-means Clustering .py
+++ K-means Clustering .py
@@ -7,18 +7,36 @@ Created on Thu Aug 27 13:12:41 2020
 
 import random
 
-training_data = [[0, 1, 0], [1, 1, 0], [2, 2, 0], [2, 1, 0], [1, 2, 0], [5, 5, 0], [5, 6, 0], [6, 6, 0], [6, 7, 0]]
+training_data = [[0, 1, 0], [1, 1, 0], [2, 2, 0], [2, 1, 0], [1, 2, 0], [5, 5, 0], [5, 6, 0], [6, 6, 0], [6, 7, 0], [3, 11, 0], [4, 12, 0], [3, 12, 0]]
 
-number_of_clusters = 2
+number_of_clusters = 3
 
 centres = []
-for i in range(number_of_clusters):
-    temp = []
-    temp.append(random.randint(0, 10))
-    temp.append(random.randint(0, 10))
-    centres.append(temp)
+point_chosen = random.randint(0, len(training_data))
+temp = []
+temp.append(training_data[point_chosen][0])
+temp.append(training_data[point_chosen][1])
+centres.append(temp)
 
-print(centres)
+while len(centres) < number_of_clusters:
+    temp_clusters = []
+    for p in range(len(training_data)):
+        if [training_data[p][0], training_data[p][1]] in centres:
+            continue
+        temp_distances = []
+        for i in range(len(centres)):
+            t = []
+            d = ((centres[i][0] - training_data[p][0])**2 + (centres[i][1] - training_data[p][1])**2)**(1/2)
+            t.append(p)
+            t.append(d)
+            temp_distances.append(t)
+        temp_distances.sort(key = lambda x: x[1])
+        temp_clusters.append(temp_distances[0])
+    temp_clusters.sort(key = lambda x: x[1], reverse=True)
+    temp = []
+    temp.append(training_data[temp_clusters[0][0]][0])
+    temp.append(training_data[temp_clusters[0][0]][1])
+    centres.append(temp)
 
 n = 10
 count = 0
@@ -32,7 +50,6 @@ while count < 10:
             temp.append(distance)
             temp_centres.append(temp)
         temp_centres.sort(key=lambda x: x[1])
-        #print(temp_centres)
         
         point[2] = temp_centres[0][0]
     
@@ -50,6 +67,5 @@ while count < 10:
         centres[i][0] = summation_x / count_x
         centres[i][1] = summation_y / count_y
     
-    print(training_data)
     count += 1
 print(training_data)
